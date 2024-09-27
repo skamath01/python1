9e813bd2f5ef049b9b245d460f8e3cb283495a65
skamath01 <skamath12345@gmail.com>
2024-09-27 13:01:21 +0530

    calculator program

---
+++ 1.py
@@ -0,0 +1,4 @@
+num1=8
+num2=4
+sum1=num1+num2
+print(sum1)
---
+++ 2.py
@@ -0,0 +1,18 @@
+
+def calc1(num1,num2):
+    
+   sum1=num1+num2
+   diff1=num1-num2
+   prd1=num1*num2
+   div1=num1/num2
+   div2=num1//num2
+   rem1=num1%num2
+   exp1=num1**num2
+   print(sum1,diff1,prd1,div1,div2,rem1,exp1)
+
+
+calc1(8,4)
+calc1(5,2)
+calc1(200,4)
+
+
